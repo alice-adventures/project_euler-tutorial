324d5760a46250fa28f9d5cc66921a26ee6e2586
Francesc Rocher <francesc.rocher@gmail.com>
2023-05-17 18:33:01 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'Project Euler support for Alice'
+copyright = '2023, Francesc Rocher'
+author = 'rocher'
 
 release = '0.1'
 version = '0.1.0'
